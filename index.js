a477083232447d6ac79a5647bedd373dda70daa5
ZHADOW999 <143715858+ZHADOW999@users.noreply.github.com>
2024-05-09 19:21:35 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const emailText = document.getElementById("emailText");
+const mysubmit = document.getElementById("mysubmit");
+const error = document.getElementById("error")
+const errorIcon = document.getElementById("errorIcon")
+var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+
+
+
+mysubmit.onclick = function(){
+    
+    if(emailText.value.trim() ==""){
+        errorIcon.style.display = "block";
+        error.textContent="You forgot to add your email";
+    }
+    else if(emailText.value.match(emailRegex)){
+        
+        error.textContent = "";
+    }  
+
+    else{
+        errorIcon.style.display = "block";
+        error.textContent = "Please provide a Valid email";
+    }
+}
